1b753dc9ec68500ec6a41b6b91c81ebcd56b32ca
eduardoarmada <135263098+eduardoarmada@users.noreply.github.com>
2023-07-12 22:22:24 +0200

    Basic flask functionalities added

--- app.py
+++ app.py
@@ -1,13 +1,29 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 from datamanager.json_data_manager import JSONDataManager
+import requests
 
+API_KEY = "24808d93"
 app = Flask(__name__)
 data_manager = JSONDataManager('movies.json')  # Use the appropriate path to your JSON file
 
 
+def request_info_movie_api(title):
+    """Obtains the information of the movie requests
+    if the movie is available in the API"""
+    movie_info = requests.get(f"http://www.omdbapi.com/",
+                              params={"apikey": API_KEY, "t": title}).json()
+    if 'Error' in movie_info:
+        if movie_info['Error'] == 'Invalid API key!':
+            raise Exception("Invalid API Key")
+        if movie_info['Error'] == 'Movie not found!':
+            raise Exception("The movie was not found")
+    return movie_info["Title"], movie_info["Year"], movie_info["imdbRating"], movie_info["Poster"], \
+        movie_info['Director']
+
+
 @app.route("/")
 def home():
-    return "hello world"
+    return jsonify("Welcome to the web app")
 
 
 @app.route('/users')
@@ -17,28 +33,45 @@ def list_users():
 
 
 @app.route("/users/<user_id>")
-def users_movies():
-    return "hello world"
+def users_movies(user_id):
+    list_of_movies_data = data_manager.get_user_movies(user_id)
+    return str(list_of_movies_data)
 
 
-@app.route("/add_user")
+@app.route("/add_user", methods=['GET', 'POST'])
 def add_user():
-    return "hello world"
+    if request.method == 'POST':
+        data_manager.add_user(request.form.get('name'))
+        return 'User added'
+    else:
+        pass
 
 
-@app.route("/users/<user_id>/add_movie")
-def add_movie():
-    return "hello world"
+@app.route("/users/<user_id>/add_movie", methods=['GET', 'POST'])
+def add_movie(user_id):
+    if request.method == 'POST':
+        data_manager.add_user_movie(user_id, request_info_movie_api(request.form.get('name')))
+        return 'Movie added'
+    else:
+        pass
 
 
-@app.route("/users/<user_id>/update_movie/<movie_id>")
-def update_movie():
-    return "hello world"
+@app.route("/users/<user_id>/update_movie/<int:movie_id>", methods=['GET', 'POST'])
+def update_movie(user_id, movie_id):
+    if request.method == 'POST':
+        data_manager.update_user_movie(user_id, movie_id, request.json)
+        return 'Movie updated'
+    else:
+        pass
 
 
-@app.route("/users/<user_id>/update_movie/<movie_id>")
-def delete_movie():
-    return "hello world"
+@app.route("/users/<user_id>/delete_movie/<int:movie_id>", methods=['GET', 'POST'])
+def delete_movie(user_id, movie_id):
+    if request.method == 'POST':
+        data_manager.delete_user_movie(user_id, movie_id)
+        return f'Movie {movie_id} deleted for user {user_id}'
+    else:
+        pass
 
 
 if __name__ == '__main__':
--- datamanager/json_data_manager.py
+++ datamanager/json_data_manager.py
@@ -12,7 +12,7 @@ class JSONDataManager(DataManagerInterface):
         with open(self.filename, 'r') as file:
             data = json.loads(file.read())
 
-        return [user.get('name', 101) for user in data.values()]
+        return [user.get('name') for user in data.values()]
 
     def get_user_movies(self, user_id):
         # Return a list of all movies for a given user
@@ -21,11 +21,60 @@ class JSONDataManager(DataManagerInterface):
 
         return data[user_id].get('movies')
 
-    def update_user_movie(self, movie_name):
-        pass
+    def add_user(self, name):
+        with open(self.filename, 'r') as file:
+            data = json.loads(file.read())
 
-    def add_user_movie(self, movie_data):
-        pass
+        users_id = max([int(user_id) for user_id in data.keys()]) + 1
+        data[str(users_id)] = {'name': name, 'movies': []}
 
-    def delete_user_movie(self, movie_name):
-        pass
+        with open(self.filename, 'w') as file:
+            file.write(json.dumps(data))
+
+    def update_user_movie(self, user_id, movie_id, movie_data):
+        user_movies = self.get_user_movies(user_id)
+        for movie in user_movies:
+            if movie['id'] == movie_id:
+                for data in movie_data.items():
+                    movie[data[0]] = data[1]
+            break
+
+        with open(self.filename, 'r') as file:
+            data = json.loads(file.read())
+
+        data[user_id]['movies'] = user_movies
+
+        with open(self.filename, 'w') as file:
+            file.write(json.dumps(data))
+
+    def add_user_movie(self, user_id, movie_data):
+        with open(self.filename, 'r') as file:
+            data = json.loads(file.read())
+
+        new_movies_id = max(int(movie['id']) for movie in self.get_user_movies(user_id)) + 1
+        new_movie = {'id': new_movies_id, 'name': movie_data[0], 'director': movie_data[-1],
+                     'year': movie_data[1], 'rating': movie_data[2], 'poster': movie_data[3]}
+
+        data[user_id]['movies'].append(new_movie)
+
+        with open(self.filename, 'w') as file:
+            file.write(json.dumps(data))
+
+    def delete_user_movie(self, user_id, movie_id):
+        user_movies = self.get_user_movies(user_id)
+        movies_to_keep = []
+
+        for movie in user_movies:
+            if movie['id'] == movie_id:
+                continue
+            movies_to_keep.append(movie)
+
+        with open(self.filename, 'r') as file:
+            data = json.loads(file.read())
+
+        data[user_id]['movies'] = movies_to_keep
+
+        with open(self.filename, 'w') as file:
+            file.write(json.dumps(data))
+
+
